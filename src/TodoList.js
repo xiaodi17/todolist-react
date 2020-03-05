23755118d6e52846bf2103b2d87fff15e8db7e05
David <xiaodi.david@gmail.com>
2020-03-06 00:42:40 +1000

    decompose components

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-
+import TodoItem from './TodoItem'
 class TodoList extends Component {
   constructor(props) {
     super(props)
@@ -23,9 +23,13 @@ class TodoList extends Component {
         <ul>
           {this.state.list.map((item, index) => {
             return (
-              <li key={index} onClick={this.handleItemDelete.bind(this, index)}>
-                {item}
-              </li>
+              <div>
+                <TodoItem
+                  content={item}
+                  index={index}
+                  deleteItem={this.handleItemDelete.bind(this)}
+                ></TodoItem>
+              </div>
             )
           })}
         </ul>
