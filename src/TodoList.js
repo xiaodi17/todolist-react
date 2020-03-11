46925e0a9cd72779129e421d9175ee891c679be6
David <xiaodi.david@gmail.com>
2020-03-11 23:42:22 +1000

    initial change using redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,90 +1,36 @@
 import React, { Component, Fragment } from 'react'
-import TodoItem from './TodoItem'
+// import TodoItem from './TodoItem'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
+import store from './store'
 class TodoList extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      inputValue: '',
-      list: []
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleBtnClick = this.handleBtnClick.bind(this)
-    this.handleItemDelete = this.handleItemDelete.bind(this)
+    this.state = store.getState()
+    console.log(store.getState())
+    // this.handleInputChange = this.handleInputChange.bind(this)
+    // this.handleBtnClick = this.handleBtnClick.bind(this)
+    // this.handleItemDelete = this.handleItemDelete.bind(this)
   }
   render() {
     return (
       <Fragment>
         <div style={{ marginTop: '10px', marginLeft: '10px' }}>
           <Input
+            value={this.state.inputValue}
             placeholder="todo info"
             style={{ width: '300px', marginRight: '10px' }}
-            id="insertArea"
-            value={this.state.inputValue}
-            onChange={this.handleInputChange}
           />
-          <Button type="primary" onClick={this.handleBtnClick}>
-            submit
-          </Button>
+          <Button type="primary">submit</Button>
         </div>
         <List
           style={{ marginTop: '10px', marginLeft: '10px', width: '300px' }}
           bordered
-          dataSource={this.getTodoItem()}
+          dataSource={this.state.list}
           renderItem={item => <List.Item>{item}</List.Item>}
         ></List>
       </Fragment>
     )
-  }
-
-  getTodoItem() {
-    return this.state.list.map((item, index) => {
-      return (
-        <TodoItem
-          key={index}
-          content={item}
-          index={index}
-          deleteItem={this.handleItemDelete}
-        />
-      )
-    })
-  }
-  handleInputChange(e) {
-    const value = e.target.value
-    this.setState(() => ({
-      inputValue: value
-    }))
-    //old way
-    // this.setState({
-    //   inputValue: e.target.value
-    // })
-  }
-  handleBtnClick() {
-    this.setState(prevState => ({
-      list: [...prevState.list, prevState.inputValue],
-      inputValue: ''
-    }))
-    //old way
-    // this.setState({
-    //   list: [...this.state.list, this.state.inputValue],
-    //   inputValue: ''
-    // })
-  }
-  handleItemDelete(index) {
-    // const list = [...this.state.list]
-    // list.splice(index, 1)
-    this.setState(prevState => {
-      const list = [...prevState.list]
-      list.splice(index, 1)
-      return {
-        list
-      }
-    })
-    //old way
-    // this.setState({
-    //   list: list
-    // })
   }
 }
 
---
+++ src/store/reducer.js
@@ -0,0 +1,7 @@
+const defaultState = {
+  inputValue: '234',
+  list: ['1', '2']
+}
+export default (state = defaultState, action) => {
+  return state
+}
