0ecc6f75bbbb428b4114b37f00d9a48925dbd758
David <xiaodi.david@gmail.com>
2020-03-12 22:35:52 +1000

    action, reducers done

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,9 +7,10 @@ class TodoList extends Component {
   constructor(props) {
     super(props)
     this.state = store.getState()
-    console.log(store.getState())
-    // this.handleInputChange = this.handleInputChange.bind(this)
-    // this.handleBtnClick = this.handleBtnClick.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    store.subscribe(this.handleStoreChange)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
     // this.handleItemDelete = this.handleItemDelete.bind(this)
   }
   render() {
@@ -20,8 +21,11 @@ class TodoList extends Component {
             value={this.state.inputValue}
             placeholder="todo info"
             style={{ width: '300px', marginRight: '10px' }}
+            onChange={this.handleInputChange}
           />
-          <Button type="primary">submit</Button>
+          <Button type="primary" onClick={this.handleBtnClick}>
+            submit
+          </Button>
         </div>
         <List
           style={{ marginTop: '10px', marginLeft: '10px', width: '300px' }}
@@ -31,6 +35,25 @@ class TodoList extends Component {
         ></List>
       </Fragment>
     )
+  }
+
+  handleInputChange(e) {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action)
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState())
+  }
+
+  handleBtnClick() {
+    const action = {
+      type: 'add_todo_item'
+    }
+    store.dispatch(action)
   }
 }
 
