bedf078d1adeda56c81b52fcd00ee1f38c0e49bf
David <xiaodi.david@gmail.com>
2020-03-12 23:05:58 +1000

    added actionCreator file

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,11 +3,12 @@ import React, { Component, Fragment } from 'react'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 import store from './store'
+// import { ADD_TODO_ITEM, DELETE_TODO_ITEM } from './store/actionTypes'
 import {
-  CHANGE_INPUT_VALUE,
-  ADD_TODO_ITEM,
-  DELETE_TODO_ITEM
-} from './store/actionTypes'
+  getInputChangeAction,
+  getAddItemAction,
+  getDeleteItemAction
+} from './store/actionCreators'
 class TodoList extends Component {
   constructor(props) {
     super(props)
@@ -46,10 +47,7 @@ class TodoList extends Component {
   }
 
   handleInputChange(e) {
-    const action = {
-      type: CHANGE_INPUT_VALUE,
-      value: e.target.value
-    }
+    const action = getInputChangeAction(e.target.value)
     store.dispatch(action)
   }
 
@@ -58,17 +56,12 @@ class TodoList extends Component {
   }
 
   handleBtnClick() {
-    const action = {
-      type: ADD_TODO_ITEM
-    }
+    const action = getAddItemAction()
     store.dispatch(action)
   }
 
   handleItemDelete(index) {
-    const action = {
-      type: DELETE_TODO_ITEM,
-      index
-    }
+    const action = getDeleteItemAction(index)
     store.dispatch(action)
   }
 }
