7777e9335c3032da726a31b3d542cc95199c77b2
David <xiaodi.david@gmail.com>
2020-03-21 13:57:44 +1000

    react-redux -WIP

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,54 +1,44 @@
 import React, { Component } from 'react'
-// import TodoItem from './TodoItem'
+import { connect } from 'react-redux'
 import 'antd/dist/antd.css'
-import store from './store'
-import TodoListUI from './TodoListUI'
-// import { ADD_TODO_ITEM, DELETE_TODO_ITEM } from './store/actionTypes'
-import {
-  getInputChangeAction,
-  getAddItemAction,
-  getDeleteItemAction
-} from './store/actionCreators'
 class TodoList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = store.getState()
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleStoreChange = this.handleStoreChange.bind(this)
-    this.handleBtnClick = this.handleBtnClick.bind(this)
-    this.handleItemDelete = this.handleItemDelete.bind(this)
-    store.subscribe(this.handleStoreChange)
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = store.getState()
+  // }
+
   render() {
     return (
-      <TodoListUI
-        inputValue={this.state.inputValue}
-        list={this.state.list}
-        handleInputChange={this.handleInputChange}
-        handleBtnClick={this.handleBtnClick}
-        handleItemDelete={this.handleItemDelete}
-      />
+      <div>
+        <input
+          value={this.props.inputValue}
+          onChange={this.props.changeInputValue}
+        ></input>
+        <button onClick={this.hand}>submit</button>
+        <ul>
+          <li>bell</li>
+        </ul>
+      </div>
     )
-  }
-
-  handleInputChange(e) {
-    const action = getInputChangeAction(e.target.value)
-    store.dispatch(action)
-  }
-
-  handleStoreChange() {
-    this.setState(store.getState())
-  }
-
-  handleBtnClick() {
-    const action = getAddItemAction()
-    store.dispatch(action)
-  }
-
-  handleItemDelete(index) {
-    const action = getDeleteItemAction(index)
-    store.dispatch(action)
   }
 }
 
-export default TodoList
+const mapStateToProps = state => {
+  return {
+    inputValue: state.inputValue
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changeInputValue(e) {
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+      }
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore } from 'redux'
 import reducer from './reducer'
-import thunk from 'redux-thunk'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-  : compose
-
-const enhancer = composeEnhancers(
-  applyMiddleware(thunk)
-  // other store enhancers if any
-)
-
-const store = createStore(reducer, enhancer)
+const store = createStore(reducer)
 
 export default store
